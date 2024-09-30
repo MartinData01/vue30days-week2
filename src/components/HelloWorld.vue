99de8c64fcbc9db650ad806ee715470164c1f1dd
Da Ming <haohwa2023@gmail.com>
2024-09-30 21:21:57 +0800

    add signIn verify function

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,9 +12,37 @@ const signupField = ref({
 })
 
 const singup = async () => {
-  console.log(`${api}/users/sign_up`)
+  // console.log(`${api}/users/sign_up`)
   const res = await axios.post(`${api}/users/sign_up`, signupField.value)
   console.log(res)
+}
+
+// 登入
+const resToken = ref('')
+const signinField = ref({
+  email: '',
+  password: ''
+})
+
+const singin = async () => {
+  // console.log(`${api}/users/sign_in`)
+  const res = await axios.post(`${api}/users/sign_in`, signinField.value)
+  console.log(res)
+  resToken.value = res.data.token
+}
+
+// 驗證
+const verifyField = ref('')
+const verifyResult = ref('')
+const verify = async () => {
+  const res = await axios.get(`${api}/users/checkout`, {
+    headers: {
+      authorization: verifyField.value
+    }
+  })
+  console.log(verifyField)
+  console.log(res)
+  verifyResult.value = res.data
 }
 </script>
 
@@ -28,11 +56,22 @@ const singup = async () => {
     <button type="button" @click="singup">註冊</button>
   </div>
   {{ signupField }}
+
   <hr />
-  <h2>登入</h2>
+  <div>
+    <h2>登入</h2>
+    <input type="email" placeholder="Email" v-model="signinField.email" />
+    <input type="text" placeholder="Password" v-model="signinField.password" />
+    <button type="button" @click="singin">登入</button>
+  </div>
+  {{ signinField }}
+  {{ resToken }}
 
   <hr />
   <h2>驗證</h2>
+  <input type="text" placeholder="Token" v-model="verifyField" />
+  <button type="button" @click="verify">驗證</button>
+  {{ verifyResult }}
 
   <hr />
   <h2>登出</h2>
